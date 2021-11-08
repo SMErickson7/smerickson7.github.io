1d4eac71785fc4b236be3ca731c7d3c971fc357e
Steven Erickson <steven.michael.erickson@gmail.com>
2021-11-08 13:25:12 -0500

    Playoff push

---
+++ playoff-bracket/playoff.js
@@ -0,0 +1,27 @@
+async function fetchplayoffJSON() {
+  const response = await fetch('playoff.json');
+  const playoff = await response.json();
+  console.log("Success");
+  return playoff;
+}
+fetchplayoffJSON().then(playoff => {
+  console.log(playoff[0].fixture.teams.home.winner); // fetched playoff
+  console.log(playoff.length);
+  for (var i = 0; i < playoff.length; i++) {
+    const x = document.getElementById(playoff[i].fixture.id);
+    const y = document.getElementById(playoff[i].fixture.id + "-path");
+    if (playoff[i].fixture.teams.home.winner == true) {
+      let imgName = "logos/" + playoff[i].fixture.teams.home.name.replace(/ /g, "_") + ".png"
+      x.setAttribute('href', imgName);
+      y.classList.remove("fixture");
+      console.log("Success");
+    } else if (playoff[i].fixture.teams.home.winner == false) {
+      let imgName = "logos/" + playoff[i].fixture.teams.away.name.replace(/ /g, "_") + ".png"
+      x.setAttribute('href', imgName);
+      y.classList.remove("fixture");
+      console.log("Success");
+    } else if (playoff[i].fixture.teams.home.winner == null) {
+      console.log("game not started")
+    }
+  }
+});
