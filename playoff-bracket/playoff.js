931d1a33727d999d5c66706272d1687f0c7beff4
Steven Erickson <steven.michael.erickson@gmail.com>
2021-11-09 13:39:11 -0500

    Update playoff.js

--- playoff-bracket/playoff.js
+++ playoff-bracket/playoff.js
@@ -9,19 +9,23 @@ fetchplayoffJSON().then(playoff => {
   console.log(playoff.length);
   for (var i = 0; i < playoff.length; i++) {
     const x = document.getElementById(playoff[i].fixture.id);
+
     const y = document.getElementById(playoff[i].fixture.id + "-path");
     if (playoff[i].fixture.teams.home.winner == true) {
       let imgName = "logos/" + playoff[i].fixture.teams.home.name.replace(/ /g, "_") + ".png"
       x.setAttribute('href', imgName);
       y.classList.remove("fixture");
       console.log("Success");
-    } else if (playoff[i].fixture.teams.home.winner == false) {
+
+    } else if (playoff[i].fixture.teams.away.winner == true) {
       let imgName = "logos/" + playoff[i].fixture.teams.away.name.replace(/ /g, "_") + ".png"
       x.setAttribute('href', imgName);
       y.classList.remove("fixture");
       console.log("Success");
+
     } else if (playoff[i].fixture.teams.home.winner == null) {
-      console.log("game not started")
+      console.log(playoff[i].fixture.teams.home.name + " vs " + playoff[i].fixture.teams.away.name + " game not started")
+
     }
   }
 });
