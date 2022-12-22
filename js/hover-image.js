71045311fb2c27d6ec64d823f84b6bc7645313bc
Steven Erickson <steven.michael.erickson@gmail.com>
2022-12-22 11:05:06 -0500

    Updates

--- js/hover-image.js
+++ js/hover-image.js
@@ -5,9 +5,20 @@ function highlightMapOn(x, l, t, h, w, br) {
   document.getElementById(x).style.width = w;
   document.getElementById(x).style.borderRadius = br;
   document.getElementById(x).style.display = "block";
-  
 }
 
 function highlightMapOff(x) {
   document.getElementById(x).style.display = "none";
+}
+
+function changeHeaderOn(x) {
+  var bodyStyles = window.getComputedStyle(document.body);
+  var fooBar = bodyStyles.getPropertyValue("--bs-strava");
+  document.getElementById(x).style.color = fooBar;
+}
+
+function changeHeaderOff(x) {
+  var bodyStyles = window.getComputedStyle(document.body);
+  var fooBar = bodyStyles.getPropertyValue("--bs-body-color");
+  document.getElementById(x).style.color = fooBar;
 }
